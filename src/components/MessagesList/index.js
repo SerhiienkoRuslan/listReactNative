349b1f32bb95df17291df43fe4666bcfce490a5c
RuslanSerhiienko <bruce.t1s@icloud.com>
2021-10-03 14:10:01 +0300

    Added messager

--- src/components/MessagesList/index.js
+++ src/components/MessagesList/index.js
@@ -3,123 +3,43 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { FlatList, RectButton } from 'react-native-gesture-handler';
 
+import { useMessageDispatch, useMessageState } from '../../context/message';
+
 import AppleStyleSwipeableRow from './AppleStyleSwipeableRow';
 
-const DATA = [
-  {
-    from: "D'Artagnan",
-    when: '3:11 PM',
-    message:
-      'Unus pro omnibus, omnes pro uno. Nunc scelerisque, massa non lacinia porta, quam odio dapibus enim, nec tincidunt dolor leo non neque',
-  },
-  {
-    from: 'Aramis',
-    when: '11:46 AM',
-    message:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus hendrerit ligula dignissim maximus aliquet. Integer tincidunt, tortor at finibus molestie, ex tellus laoreet libero, lobortis consectetur nisl diam viverra justo.',
-  },
-  {
-    from: 'Athos',
-    when: '6:06 AM',
-    message:
-      'Sed non arcu ullamcorper, eleifend velit eu, tristique metus. Duis id sapien eu orci varius malesuada et ac ipsum. Ut a magna vel urna tristique sagittis et dapibus augue. Vivamus non mauris a turpis auctor sagittis vitae vel ex. Curabitur accumsan quis mauris quis venenatis.',
-  },
-  {
-    from: 'Porthos',
-    when: 'Yesterday',
-    message:
-      'Vivamus id condimentum lorem. Duis semper euismod luctus. Morbi maximus urna ut mi tempus fermentum. Nam eget dui sed ligula rutrum venenatis.',
-  },
-  {
-    from: 'Domestos',
-    when: '2 days ago',
-    message:
-      'Aliquam imperdiet dolor eget aliquet feugiat. Fusce tincidunt mi diam. Pellentesque cursus semper sem. Aliquam ut ullamcorper massa, sed tincidunt eros.',
-  },
-  {
-    from: 'Cardinal Richelieu',
-    when: '2 days ago',
-    message:
-      'Pellentesque id quam ac tortor pellentesque tempor tristique ut nunc. Pellentesque posuere ut massa eget imperdiet. Ut at nisi magna. Ut volutpat tellus ut est viverra, eu egestas ex tincidunt. Cras tellus tellus, fringilla eget massa in, ultricies maximus eros.',
-  },
-  {
-    from: "D'Artagnan",
-    when: 'Week ago',
-    message:
-      'Aliquam non aliquet mi. Proin feugiat nisl maximus arcu imperdiet euismod nec at purus. Vestibulum sed dui eget mauris consequat dignissim.',
-  },
-  {
-    from: 'Cardinal Richelieu',
-    when: '2 weeks ago',
-    message:
-      'Vestibulum ac nisi non augue viverra ullamcorper quis vitae mi. Donec vitae risus aliquam, posuere urna fermentum, fermentum risus. ',
-  },
-  {
-    from: "D'Artagnan",
-    when: '3:11 PM',
-    message:
-      'Unus pro omnibus, omnes pro uno. Nunc scelerisque, massa non lacinia porta, quam odio dapibus enim, nec tincidunt dolor leo non neque',
-  },
-  {
-    from: 'Aramis',
-    when: '11:46 AM',
-    message:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus hendrerit ligula dignissim maximus aliquet. Integer tincidunt, tortor at finibus molestie, ex tellus laoreet libero, lobortis consectetur nisl diam viverra justo.',
-  },
-  {
-    from: 'Athos',
-    when: '6:06 AM',
-    message:
-      'Sed non arcu ullamcorper, eleifend velit eu, tristique metus. Duis id sapien eu orci varius malesuada et ac ipsum. Ut a magna vel urna tristique sagittis et dapibus augue. Vivamus non mauris a turpis auctor sagittis vitae vel ex. Curabitur accumsan quis mauris quis venenatis.',
-  },
-  {
-    from: 'Porthos',
-    when: 'Yesterday',
-    message:
-      'Vivamus id condimentum lorem. Duis semper euismod luctus. Morbi maximus urna ut mi tempus fermentum. Nam eget dui sed ligula rutrum venenatis.',
-  },
-  {
-    from: 'Domestos',
-    when: '2 days ago',
-    message:
-      'Aliquam imperdiet dolor eget aliquet feugiat. Fusce tincidunt mi diam. Pellentesque cursus semper sem. Aliquam ut ullamcorper massa, sed tincidunt eros.',
-  },
-  {
-    from: 'Cardinal Richelieu',
-    when: '2 days ago',
-    message:
-      'Pellentesque id quam ac tortor pellentesque tempor tristique ut nunc. Pellentesque posuere ut massa eget imperdiet. Ut at nisi magna. Ut volutpat tellus ut est viverra, eu egestas ex tincidunt. Cras tellus tellus, fringilla eget massa in, ultricies maximus eros.',
-  },
-  {
-    from: "D'Artagnan",
-    when: 'Week ago',
-    message:
-      'Aliquam non aliquet mi. Proin feugiat nisl maximus arcu imperdiet euismod nec at purus. Vestibulum sed dui eget mauris consequat dignissim.',
-  },
-  {
-    from: 'Cardinal Richelieu',
-    when: '2 weeks ago',
-    message:
-      'Vestibulum ac nisi non augue viverra ullamcorper quis vitae mi. Donec vitae risus aliquam, posuere urna fermentum, fermentum risus. ',
-  }
-];
-
-const MessagesList = ({ items, handleEdit, handleDelete }) => {
+const MessagesList = () => {
   const [isSwiping, setSwiping] = useState(false);
+  const dispatch = useMessageDispatch();
+  const { users } = useMessageState();
 
-  const Row = ({ item }) => (
-    <RectButton style={styles.rectButton} onPress={() => Alert.alert(item.from)}>
-      <Text style={styles.fromText}>
-        {item.from}
-      </Text>
+  const selectedUser = users?.find((u) => u.selected === true)?.username;
 
-      <Text numberOfLines={2} style={styles.messageText}>
-        {item.message}
-      </Text>
+  const Row = ({ item }) => {
+    const selected = selectedUser === item.username;
 
-      <Text style={styles.dateText}>{item.when} ❭</Text>
-    </RectButton>
-  );
+    return (
+      <RectButton
+        style={styles.rectButton}
+        onPress={() => {
+          console.log('-pr')
+          dispatch({ type: 'SET_SELECTED_USER', payload: item.username });
+        }}
+      >
+        <Text style={styles.fromText}>
+          {item?.username || ''}
+        </Text>
+
+        <Text numberOfLines={2} style={styles.messageText}>
+          {item?.latestMessage?.content || ''}
+        </Text>
+
+        <Text style={styles.dateText}>
+          {item?.latestMessage?.createdAt ? new Date(item?.latestMessage?.createdAt).toISOString().split('T')[0] : ''}
+          {' '}❭
+        </Text>
+      </RectButton>
+    )
+  };
 
   const ItemSwipeable = (item) => (
     <AppleStyleSwipeableRow>
@@ -131,15 +51,11 @@ const MessagesList = ({ items, handleEdit, handleDelete }) => {
     <FlatList
       ItemSeparatorComponent={() => <View style={styles.separator} />}
       style={{ flexGrow: 1 }}
-      data={items || DATA || []}
+      data={users || []}
       scrollEnabled={!isSwiping}
       renderItem={({ item }) => ItemSwipeable(item)}
     />
   );
-};
-
-MessagesList.defaultProps = {
-  items: []
 };
 
 const styles = StyleSheet.create({
@@ -150,11 +66,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     justifyContent: 'space-between',
     flexDirection: 'column',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    color: '#000'
   },
   separator: {
     backgroundColor: 'rgb(200, 199, 204)',
-    height: StyleSheet.hairlineWidth
+    height: 1
   },
   fromText: {
     fontWeight: 'bold',
--- src/screens/MessageListScreen/index.js
+++ src/screens/MessageListScreen/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { gql, useQuery } from '@apollo/client';
+
 import MessagesList from 'components/MessagesList';
+import Loading from 'components/Loading';
+import { useMessageDispatch, useMessageState } from '../../context/message';
+
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      username
+      createdAt
+      latestMessage {
+        uuid
+        from
+        to
+        content
+        createdAt
+      }
+    }
+  }
+`
 
 const MessageListScreen = () => {
+  const dispatch = useMessageDispatch();
+  const { users } = useMessageState();
+
+  let usersMarkup;
+
+  const { loading } = useQuery(GET_USERS, {
+    onCompleted: (data) =>
+      dispatch({ type: 'SET_USERS', payload: data.getUsers }),
+    onError: (err) => console.log(err)
+  });
+
+  if (!users || loading) {
+    usersMarkup = <Loading />
+  } else if (users.length === 0) {
+    usersMarkup = <Text>No users have joined yet</Text>
+  } else if (users.length > 0) {
+    usersMarkup = <MessagesList />
+  }
+
   return (
     <View style={{ flexGrow: 1 }}>
-      <MessagesList
-        handleEdit={() => {}}
-        handleDelete={() => {}}
-        items={null}
-      />
+      {usersMarkup}
     </View>
   );
 };
